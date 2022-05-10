76c01ea7d6a294e8a639c7379ccd9ef8221d5be2
Toever1503 <Haunv1503@gmail.com>
2022-05-10 15:45:57 +0700

    fa

--- src/main/java/com/entity/dto/ResponseDto.java
+++ src/main/java/com/entity/dto/ResponseDto.java
@@ -5,19 +5,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class ResponseDto implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    private String version = "1.0";
     private String result;
     private Object data;
     private String message;
+
+    public ResponseDto(String s, Object data, String s1) {
+        this.result = s;
+        this.data = data;
+        this.message = s1;
+    }
 
     public static ResponseDto ofCreated(Object data) {
         return new ResponseDto(data != null ? "SUCCESS" : "ERROR", data, data != null ? "Created successfully!" : "Created failed!");
